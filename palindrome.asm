ea8692f3918b439baef8d21f1fc54053d41b3911
Nguyen Tran <60759653+nctran108@users.noreply.github.com>
2022-09-15 13:09:19 -0400

    fixed issues of print out wrong output

--- palindrome.asm
+++ palindrome.asm
@@ -9,45 +9,54 @@ buffer: .space 20
 .globl main
 
 main:
-	# print string ask for input
+	# print string1 to ask for input
 	li $v0,4
 	la $a0,string1
 	syscall
 	
-	# input your string
+	# input your string to buffer address
 	li $v0,8
 	la $a0,buffer
 	la $a1,20
 	syscall
 			
 	jal strlen
-	# $t1 (j) = strlen(A[],max_size)
+	# $t1 (j) = strlen(buffer[],max_size)
 	move $t1,$v0
+	# j = j - 1
 	addi $t1,$t1,-1
 	# $t0 (i) = 0
 	li $t0,0
 while:
-	# while(i<j)
+	# while(i<j) and does not care index i = j because it is same character
 	bge $t0,$t1,end
-	# get A[i} character
+	# get character in index i from buffer and load it to $t4
 	add $t2,$a0,$t0
 	lb $t4,0($t2)
-	# get A[j] character
+	# get character in j index from buffer and load it to $t5
 	add $t3,$a0,$t1
 	lb $t5,0($t3)
 	
+	# if($t4==$t5) then jump to continous label to have i++ and j-- and loop back
 	beq $t4,$t5,continous
 	
+	# else just print out string3 since both char are not the same
 	li $v0,4
 	la $a0,string3
 	syscall
 	
+	# jump to exit the program
 	j exit
 continous:
+	# i++
 	addi $t0,$t0,1
+	# j--
 	addi $t1,$t1,-1
+	# re-loop
 	j while
 end:	
+	# if the while successful then it is palindrome string
+	# so print string2 out
 	li $v0,4
 	la $a0,string2
 	syscall	
@@ -61,19 +70,28 @@ strlen:
 	addi $sp,$sp,-4
 	sw $s0,0($sp)
 	
+	# assign temp ($t5) = string array ($a0)
+	move $t5,$a0
+	
 	# count = 0
 	li $t0,0
 loop:
-	lb $t1,0($a0)	
+	# load each char (byte) in $t5 string
+	lb $t1,0($t5)	
 	
+	# check if char is zero which right after Null char
+	# if true then stop the loop
 	beqz $t1,done
 	
+	# else count++
 	addi $t0,$t0,1
-	addi $a0,$a0,1
+	# increment $t5 address by 1-byte each loop
+	addi $t5,$t5,1
 	j loop
 done:
+	# because after string always have neuline (\n)
+	# so return count - 1 to uncount the newline
 	addi $v0,$t0,-1
 	lw $s0,0($sp)
 	addi $sp,$sp,4
 	jr $ra
-	
